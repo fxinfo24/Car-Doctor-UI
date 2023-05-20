15dcb911ae863738abc4968fe67340ac7fe50783
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-05-20 23:44:58 +0600

    NavBar items added, Banner Component created with Daisy and Tailwind CSS, and so...

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,102 @@
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
+
+const Banner = () => {
+  return (
+    <div className="carousel w-full h-[600px]">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={img1} className="w-full rounded-xl" />
+        {/* Add text/button over image */}
+        <div className="absolute flex items-center h-full bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+          <div className="text-white  space-y-4 pl-24">
+            <h3 className="text-6xl w-96">
+              Affordable Price for car servicing
+            </h3>
+            <p className="w-2/3">
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div className="flex gap-5">
+              <button className="btn btn-active btn-error">
+                Discover More
+              </button>
+              <button className="btn btn-outline btn-info">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Slider buttons */}
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide6" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={img2} className="w-full" />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={img3} className="w-full" />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src={img4} className="w-full" />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide5" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide5" className="carousel-item relative w-full">
+        <img src={img5} className="w-full" />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide6" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide6" className="carousel-item relative w-full">
+        <img src={img6} className="w-full" />
+        <div className="absolute flex justify-end gap-5 transform -translate-y-1/2 left-5 right-5 bottom-10">
+          <a href="#slide5" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import NavBar from "../../Shared/NavBar/NavBar";
+import Banner from "../Banner/Banner";
+
 
 const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+            <Banner></Banner>
             <h2>This is home</h2>
         </div>
     );
